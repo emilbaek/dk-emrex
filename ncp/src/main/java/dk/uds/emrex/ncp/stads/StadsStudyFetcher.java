fde55f6174c0d2d7ce95b4f098c5fc05b3fa58e1
Qvazar <sebjorg@gmail.com>
2016-08-29 11:51:21 +0200

    STADS SOAP WS client implemented

--- ncp/src/main/java/dk/uds/emrex/ncp/stads/StadsStudyFetcher.java
+++ ncp/src/main/java/dk/uds/emrex/ncp/stads/StadsStudyFetcher.java
@@ -1,57 +1,32 @@
 package dk.uds.emrex.ncp.stads;
 
-import dk.kmd.emrex.common.UrlBuilder;
 import dk.kmd.emrex.common.idp.IdpConfig;
 import dk.kmd.emrex.common.idp.IdpConfigListService;
 import dk.uds.emrex.ncp.StudyFetcher;
-import org.apache.commons.collections.BufferUnderflowException;
+import dk.uds.emrex.stads.wsdl.GetStudentsResultInput;
+import dk.uds.emrex.stads.wsdl.GetStudentsResults;
+import dk.uds.emrex.stads.wsdl.GetStudentsResultsResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
+import org.springframework.ws.WebServiceException;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.SoapFaultClientException;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
 
 import javax.validation.constraints.NotNull;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
+import javax.xml.transform.stream.StreamResult;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
+import java.io.StringWriter;
+import java.math.BigInteger;
 import java.nio.ByteBuffer;
-import java.util.Base64;
 import java.util.UUID;
 
 /**
  * Created by sj on 30-03-16.
  */
-public class StadsStudyFetcher implements StudyFetcher {
-    private static class StadsResponse {
-        private UUID requestId;
-        private String elmoDocument;
-
-        public StadsResponse(UUID requestId, String elmoDocument) {
-            this.requestId = requestId;
-            this.elmoDocument = elmoDocument;
-        }
-
-        public UUID getRequestId() {
-            return requestId;
-        }
-
-        public String getElmoDocument() {
-            return elmoDocument;
-        }
-    }
-
+public class StadsStudyFetcher extends WebServiceGatewaySupport implements StudyFetcher {
     private static Logger LOG = LoggerFactory.getLogger(StadsStudyFetcher.class);
 
     @Autowired
@@ -77,80 +52,80 @@ public class StadsStudyFetcher implements StudyFetcher {
 
     private String fetchStudies(Iterable<IdpConfig.IdpConfigUrl> urls, String ssn) throws IOException {
         for (IdpConfig.IdpConfigUrl idpConfigUrl : urls) {
-            final UUID requestId = createRequestId();
 
-            final URL url = new UrlBuilder(idpConfigUrl.getUrl())
-                    .setParameter("requestID", uuidToString(requestId))
-                    .setParameter("cpr", ssn)
-                    .toUrl();
-
+            final String url = idpConfigUrl.getUrl();
             LOG.info("Opening connection to STADS with URL {}", url);
 
-            final URLConnection urlConnection = url.openConnection();
-            urlConnection.setConnectTimeout(this.connectionTimeout);
-
             try {
-                urlConnection.connect();
-            } catch (IOException e) {
-                // Unable to connect, try next server
-                continue;
-            }
-
-            try (final InputStream resStream = urlConnection.getInputStream()) {
-                // TODO Read actual XML response and validate it
-                final StadsResponse stadsResponse = parseResponse(resStream);
-                if (!stadsResponse.getRequestId().equals(requestId)) {
-                    throw new IOException(String.format("Invalid requestId in response! Got {}, expected {}", stadsResponse.getRequestId(), requestId));
-                }
-
-                return stadsResponse.getElmoDocument();
+                return getStudentsResults(url, ssn);
+            } catch (IOException | WebServiceException ex) {
+                LOG.warn(String.format("Error when connecting to STADS web-service at {}.", url), ex);
             }
         }
 
         // If we get here we were unable to connect and/or read a response from a STADS server.
         // Throw error: Not able to connect to any STADS URL
-        throw new IOException("Unable to connect to any URL in list.");
+        throw new IOException(String.format("Unable to successfully get data from any STADS URL in list."));
     }
 
-    private  StadsResponse parseResponse(InputStream resStream) throws IOException {
-        try {
-            final Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(resStream);
-            final XPath xPath = XPathFactory.newInstance().newXPath();
-
-            final XPathExpression requestIdXPath = xPath.compile("");
-            final XPathExpression elmoXPath = xPath.compile("");
-
-            final UUID requestId = uuidFromString(requestIdXPath.evaluate(doc));
-            final String elmo = elmoXPath.evaluate(doc);
-
-            return new StadsResponse(requestId, elmo);
-        } catch (ParserConfigurationException | SAXException | XPathExpressionException e) {
-            throw new IOException(e);
-        }
-    }
-
-    private static UUID createRequestId() {
-        return UUID.randomUUID();
-    }
-
-    private static String uuidToString(@NotNull UUID uuid) {
+    private static BigInteger uuidToBigInteger(@NotNull UUID uuid) {
         final ByteBuffer buffer = ByteBuffer.allocate(16);
 
         buffer.putLong(uuid.getMostSignificantBits());
         buffer.putLong(uuid.getLeastSignificantBits());
 
-        return Base64.getEncoder().encodeToString(buffer.array());
+        return new BigInteger(buffer.array());
     }
 
-    private static UUID uuidFromString(@NotNull String string) throws IOException {
-        try {
-            final byte[] bytes = Base64.getDecoder().decode(string);
-            final ByteBuffer buffer = ByteBuffer.wrap(bytes);
+    /***
+     *
+     * @param url
+     * @param cpr
+     * @return ELMO XML as String
+     * @throws IOException
+     * @throws SoapFaultClientException
+     */
+    private String getStudentsResults(String url, String cpr) throws IOException, WebServiceException {
+        final BigInteger requestId = uuidToBigInteger(UUID.randomUUID());
 
-            return new UUID(buffer.getLong(), buffer.getLong());
-        } catch (IllegalArgumentException | BufferUnderflowException e) {
-            throw new IOException(e);
+        final GetStudentsResults request = new GetStudentsResults();
+
+        final GetStudentsResultInput input = new GetStudentsResultInput();
+        input.setCPR(formatCprToStads(cpr));
+        input.setRequestId(requestId);
+
+        request.setInputStruct(new GetStudentsResultInput());
+
+        final GetStudentsResultsResponse response = (GetStudentsResultsResponse) this.getWebServiceTemplate()
+                .marshalSendAndReceive(
+                        url,
+                        request,
+                        new SoapActionCallback(url)
+                );
+
+        final GetStudentsResultsResponse.ReceiptStructure receipt = response.getReceiptStructure();
+
+        switch (receipt.getReceiptCode()) {
+            case 0:
+                // Get ELMO document as XML string
+                final StringWriter xmlWriter = new StringWriter();
+                final StreamResult marshalResult = new StreamResult(xmlWriter);
+
+                this.getMarshaller().marshal(response.getElmoDocument(), marshalResult);
+
+                return xmlWriter.toString();
+            default:
+                throw new IOException(String.format("STADS error: {0} - {1}", receipt.getReceiptCode(), receipt.getReceiptText()));
         }
+    }
 
+    private static String formatCprToStads(@NotNull String cpr) {
+        if (cpr.length() == 10) {
+            return cpr.substring(0, 6) + "-" + cpr.substring(6);
+        } else if (cpr.length() == 11) {
+            return cpr;
+        } else {
+            throw new IllegalArgumentException("cpr must be either length 10 or 11");
+        }
     }
 }
