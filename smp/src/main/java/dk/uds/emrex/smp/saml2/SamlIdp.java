d33e99728fa57cbf4e344d4cb71f728782e8db42
Z8QNK <Z8QNK@H47817.intern.kmd.dk>
2016-11-08 15:29:18 +0100

    stads endpoint og ToString

--- smp/src/main/java/dk/uds/emrex/smp/saml2/SamlIdp.java
+++ smp/src/main/java/dk/uds/emrex/smp/saml2/SamlIdp.java
@@ -1,8 +1,11 @@
 package dk.uds.emrex.smp.saml2;
 
+import lombok.ToString;
+
 /**
  * Created by sj on 23-05-16.
  */
+@ToString
 public class SamlIdp {
     private String id;
     private String name;
