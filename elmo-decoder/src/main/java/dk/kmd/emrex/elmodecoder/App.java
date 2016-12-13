ff59fe37cb917237e706525e8fd514043adc1e61
Emil Østergaard Bæk <emb@kmd.dk>
2016-12-13 10:50:58 +0100

    Opdatering af vejledning i elmodecoder

--- elmo-decoder/src/main/java/dk/kmd/emrex/elmodecoder/App.java
+++ elmo-decoder/src/main/java/dk/kmd/emrex/elmodecoder/App.java
@@ -17,28 +17,30 @@ public class App {
 			commandUsage();
 		}
 		try {
-			String fileContent = Files.lines(Paths.get(args[0])).reduce((a,b)->a+b).orElse("");
+			String fileContent = Files.lines(Paths.get(args[0])).reduce((a, b) -> a + b).orElse("");
 			byte[] decodedElmo = Base64.getDecoder().decode(fileContent);
 			decodedElmo = uncompressGzip(decodedElmo);
-			Pattern pattern = Pattern.compile("content[^>]*>.*base64,([^<]*)</content>"); 
-			Matcher matcher = pattern.matcher((new String(decodedElmo)).replace("\n","").replace("\r","")); 
-			boolean found = matcher.find(); 
-			if (found){
-				byte[] decodedPDF = Base64.getDecoder().decode(matcher.group(1)); 
-				try(FileOutputStream fos = new FileOutputStream("out.pdf")){
+			Pattern pattern = Pattern.compile("content[^>]*>.*base64,([^<]*)</content>");
+			Matcher matcher = pattern.matcher((new String(decodedElmo)).replace("\n", "").replace("\r", ""));
+			boolean found = matcher.find();
+			if (found) {
+				byte[] decodedPDF = Base64.getDecoder().decode(matcher.group(1));
+				try (FileOutputStream fos = new FileOutputStream("out.pdf")) {
 					fos.write(decodedPDF);
 					fos.close();
 					System.out.println("Output written to file out.pdf");
-				}catch(IOException ioe){
+				} catch (IOException ioe) {
 					System.out.println("Unable to write to the file out.pdf");
 				}
+			} else {
+				System.out.println("Unable to find pattern " + pattern.pattern() + " in base64 encoded content");
 			}
 		} catch (IOException e) {
 			System.out.println("An error happened");
 			commandUsage();
 		}
 	}
-	
+
 	private static byte[] uncompressGzip(byte[] in) throws IOException {
 		ByteArrayInputStream bytein = new ByteArrayInputStream(in);
 		GZIPInputStream gzin = new GZIPInputStream(bytein);
@@ -46,16 +48,16 @@ public class App {
 		int res = 0;
 		byte buf[] = new byte[1024];
 		while (res >= 0) {
-		    res = gzin.read(buf, 0, buf.length);
-		    if (res > 0) {
-		        byteout.write(buf, 0, res);
-		    }
+			res = gzin.read(buf, 0, buf.length);
+			if (res > 0) {
+				byteout.write(buf, 0, res);
+			}
 		}
 		byte uncompressed[] = byteout.toByteArray();
-		return uncompressed; 
+		return uncompressed;
 	}
-	
-	private static void commandUsage(){
+
+	private static void commandUsage() {
 		System.out.println("Command usage java -jar elmo-decoder.jar <filename>");
 		System.out.println("File is suppposed to be a base64 encoded text file");
 		System.exit(0);
